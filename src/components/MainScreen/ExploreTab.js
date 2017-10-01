ec5bfd9f7a321df2fccb0925344bac1d51d482ab
yutoJ <yutoogata4@gmail.com>
2017-10-01 18:48:10 +0900

    create room reducer

--- src/components/MainScreen/ExploreTab.js
+++ src/components/MainScreen/ExploreTab.js
@@ -35,54 +35,18 @@ const styles = StyleSheet.create({
   }
 });
 
-const items = [
-  {
-    id: 1,
-    title: 'New York',
-    homeType: 'House',
-    image: {uri: 'https://i.pinimg.com/originals/47/f9/36/47f936a0115aa0ee91ee53b2f85bf90e.jpg'},
-    bedRoom: 2,
-    price: 150,
-    instant: true
-  },
-  {
-    id: 2,
-    title: 'Melbourne',
-    homeType: 'Apartment',
-    image: {uri: 'https://cdn.trendhunterstatic.com/thumbs/nettleton-198-by-saota.jpeg'},
-    bedRoom: 3,
-    price: 250,
-    instant: false
-  },
-  {
-    id: 3,
-    title: 'Paris',
-    homeType: 'House',
-    image: {uri: 'http://cdn.trendir.com/wp-content/uploads/2016/09/Glass-Graham-House-by-E.-Cobb-Architects-900x751.jpg'},
-    bedRoom: 1,
-    price: 99,
-    instant: true
-  },
-];
-
 class ExploreTab extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: items
-    };
-  }
 
   onPress(item) {
     this.props.navigate({ routeName: "Detail", params: { item: item } });
   }
 
   render() {
+    const { rooms } = this.props;
     return (
       <FlatList
         style={styles.container}
-        data={this.state.items}
+        data={rooms}
         renderItem={({item}) =>
           <TouchableOpacity onPress={() => this.onPress(item)} style={styles.item}>
             <Image style={styles.image} source= {item.image} />
@@ -97,7 +61,7 @@ class ExploreTab extends Component {
 }
 
 const mapStateToProps = state => ({
-
+  rooms: state.room.rooms
 });
 
 const mapDispatchToProps = dispatch => ({
